a54770036a4d3661bb30cd6649868521083e9478
Kavya <kavyakopatti99@gmail.com>
2026-04-29 14:58:55 +0530

    fix: use direct method references in auth routes

--- server/src/interface/Http/routes/authRoutes.ts
+++ server/src/interface/Http/routes/authRoutes.ts
@@ -17,46 +17,16 @@ import {
 } from "../../../infrastructure/config/di";
 const router = Router();
 
-router.post("/register", validateUserInput, (req: Request, res: Response) => {
-  registerController.handle(req, res);
-});
-
-router.post("/pre-signup", validateUserInput, (req: Request, res: Response) => {
-  preSignupController.handle(req, res);
-});
-
-router.post("/google-login", googleLoginController.handle);
-router.post("/verify-otp", (req: Request, res: Response) =>
-  otpController.verifyOtp(req, res),
-);
-router.post("/send-otp", (req: Request, res: Response) =>
-  otpController.sendOtp(req, res),
-);
-router.post("/resend-otp", (req: Request, res: Response) =>
-  otpController.resendOtp(req, res),
-);
-router.post("/complete-signup", (req: Request, res: Response) =>
-  completeSignupController.handle(req, res),
-);
-
-router.post("/login", (req: Request, res: Response) => {
-  loginController.handle(req, res);
-});
-router.post("/logout", (req: Request, res: Response) =>
-  logoutController.handle(req, res),
-);
-router.post("/refresh", (req: Request, res: Response) =>
-  refreshTokenController.handle(req, res),
-);
-
-router.post("/forgotPassword", validateEmail, (req: Request, res: Response) => {
-  resetPasswordController.forgotpassword(req, res);
-});
-router.patch(
-  "/resetPassword",
-  validatePassword,
-  (req: Request, res: Response) =>
-    resetPasswordController.resetPassword(req, res),
-);
-
+router.post("/login", loginController.handle.bind(loginController));
+router.post("/register", validateUserInput, registerController.handle.bind(registerController));
+router.post("/pre-signup", validateUserInput, preSignupController.handle.bind(preSignupController));
+router.post("/complete-signup", completeSignupController.handle.bind(completeSignupController));
+router.post("/logout", logoutController.handle.bind(logoutController));
+router.post("/refresh", refreshTokenController.handle.bind(refreshTokenController));
+router.post("/forgotPassword", validateEmail, resetPasswordController.forgotpassword.bind(resetPasswordController));
+router.patch("/resetPassword", validatePassword, resetPasswordController.resetPassword.bind(resetPasswordController));
+router.post("/google-login", googleLoginController.handle.bind(googleLoginController));
+router.post("/verify-otp", otpController.verifyOtp.bind(otpController));
+router.post("/send-otp", otpController.sendOtp.bind(otpController));
+router.post("/resend-otp", otpController.resendOtp.bind(otpController));
 export default router;
